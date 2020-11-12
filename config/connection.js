71622773d2298327cf3de95badfa68f1e9daa1b4
Tom Chestnut <tchestnut85@gmail.com>
2020-11-11 23:37:49 -0500

    added a sequelize config connection, user model and association in models/index. also made facebook api fetch function.

---
+++ config/connection.js
@@ -0,0 +1,14 @@
+// import sequelize constructor and dotenv
+const Sequelize = require('sequelize');
+require('dotenv').config();
+
+// create the database connection
+const sequelize = process.env.JAWSDB_URL
+    ? new Sequelize(process.env.JAWSDB_URL)
+    : new Sequelize(process.env.DB_NAME, process.env.DB_USER, process.env.DB_PW, {
+        host: 'localhost',
+        dialect: 'mysql',
+        port: 3306
+    });
+
+module.exports = sequelize;

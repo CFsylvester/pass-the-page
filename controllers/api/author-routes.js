09246d561b49f48036dca8ad8a6b5d06ee5d7c50
Tom Chestnut <tchestnut85@gmail.com>
2020-11-14 00:16:32 -0500

    added author routes to update and delete an author and login/logout routes

--- controllers/api/author-routes.js
+++ controllers/api/author-routes.js
@@ -60,11 +60,89 @@ router.post('/', (req, res) => {
 });
 
 // Login route
-router.get('/login', (req, res) => {
-    console.log('==================');
-    res.render('login');
+router.post('/login', (req, res) => {
+    console.log(`====== Logged in as ${req.session.username}! ======`);
+    Author.findOne({
+        where: {
+            username: req.body.username
+        }
+    })
+        .then(authorData => {
+            if (!authorData) {
+                res.status(404).json({ message: `${authorData.username} isn't an existing user.` });
+                return;
+            }
+
+            const validPw = authorData.checkPassword(req.body.password);
+
+            if (!validPw) {
+                res.status(400).json({ message: "Wrong password!" });
+            }
+
+            req.session.save(() => {
+                req.ssession.author_id = authorData.author_id;
+                req.sesssion.username = authorData.username;
+                req.session.loggedIn = true;
+
+                res.json({ user: authorData, message: `Logged in as ${authorData.username}.` });
+            });
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
 });
 
+// Log out user
+router.post('/logout', (req, res) => {
+    if (req.session.loggedIn) {
+        req.session.destroy(() => {
+            console.log(`${req.session.username} logged out!`);
+            res.status(204).end();
+        });
+    } else {
+        res.status(404).end();
+    }
+});
 
+// Update a user's info
+router.put('/:id', (req, res) => {
+    Author.update(req.body, {
+        individualHooks: true,
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(authorData => {
+            if (!authorData) {
+                res.status(404).json({ message: "We couldn't find that author." });
+                return;
+            }
+            res.json(authorData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
+
+// Delete a user
+router.delete('/:id', (req, res) => {
+    Author.destroy({
+        where: {
+            id: req.params.id
+        }
+    })
+        .then(authorData => {
+            if (!authorData) {
+                res.status(404).json({ message: "We couldn't find that author." });
+            }
+            res.json(authorData);
+        })
+        .catch(err => {
+            console.log(err);
+            res.status(500).json(err);
+        });
+});
 
 module.exports = router;

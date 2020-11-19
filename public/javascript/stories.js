9de674b6f9f9626b2ae407f5d646b5746c78640e
Tom Chestnut <tchestnut85@gmail.com>
2020-11-19 17:19:19 -0500

    updated stories.js and IDs to story-card.handlebars to use for variables in stories.js.

--- public/javascript/stories.js
+++ public/javascript/stories.js
@@ -1,5 +1,31 @@
-const natural = require('../../utils/natural');
+const analyzeText = require('../../utils/natural');
 
-async function getStories() {
+const genreImg = document.getElementById(`#image-${id}`);
+const story = document.getElementById(`#story-${id}`).textContent;
 
+function sentiment(story) {
+    analyzeText(story);
+
+    console.log(value);
+    return value;
+}
+
+
+
+switch (sentiment(story)) {
+    case (value < -3):
+        genreImg.setAttribute('src', '/src/adventure.png');
+        break;
+    case (value > -3 && value < -1):
+        genreImg.setAttribute('src', '/src/adventure.png');
+        break;
+    case (value > -1 && value < 1):
+        genreImg.setAttribute('src', '/src/adventure.png');
+        break;
+    case (value > 1 && value < 3):
+        genreImg.setAttribute('src', '/src/adventure.png');
+        break;
+    case (value > 3 && value < 5):
+        genreImg.setAttribute('src', '/src/adventure.png');
+        break;
 }

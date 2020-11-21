9318a0311a03548f38817ac6125bccb3ce9fe17a
Tom Chestnut <tchestnut85@gmail.com>
2020-11-21 18:48:54 -0500

    updated stories.js and css to use sentiment value to change story card backgrounds.

--- public/javascript/stories.js
+++ public/javascript/stories.js
@@ -8,26 +8,37 @@
 function storyTone() {
 
     const storyCards = document.getElementsByClassName('.card-body');
-    const sentValue = storyCards.getAttribute('data-sentiment');
 
-    console.log('storyCArds:', storyCards);
 
-    switch (sentValue) {
-        case (sentValue < -3):
+    for (let i = 0; i < storyCards.length; i++) {
+        const story = storyCards[i];
+        const sentValue = story.getAttribute('data-sentiment');
+        console.log('sentValue', sentValue);
+
+        if ((sentValue < -3) {
             storyText.classList.add('gradient1');
-            break;
-        case (sentValue > -3 && sentValue < -1):
-            storyText.classList.add('gradient2');
-            break;
-        case (sentValue > -1 && sentValue < 1):
-            storyText.classList.add('gradient3');
-            break;
-        case (sentValue > 1 && sentValue < 3):
-            storyText.classList.add('gradient4');
-            break;
-        case (sentValue > 3 && sentValue < 5):
-            storyText.classList.add('gradient5');
-            break;
+        } else if ()
+
+
+
+
+            switch (sentValue) {
+                case (sentValue < -3):
+                    storyText.classList.add('gradient1');
+                    break;
+                case (sentValue > -3 && sentValue < -1):
+                    storyText.classList.add('gradient2');
+                    break;
+                case (sentValue > -1 && sentValue < 1):
+                    storyText.classList.add('gradient3');
+                    break;
+                case (sentValue > 1 && sentValue < 3):
+                    storyText.classList.add('gradient4');
+                    break;
+                case (sentValue > 3 && sentValue < 5):
+                    storyText.classList.add('gradient5');
+                    break;
+            }
     }
 }
 

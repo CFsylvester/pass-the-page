ca8ee73ce76063deb13766c90bd31d2e5379993e
Tom Chestnut <tchestnut85@gmail.com>
2020-11-21 22:23:50 -0500

    finished storyTone function in stories.js to change story card backgrounds based on sentiment data value.

--- public/javascript/stories.js
+++ public/javascript/stories.js
@@ -1,51 +1,24 @@
-// const analyzeText = require('../../utils/natural');
-
-// const card = document.getElementById(`#card-${id}`);
-// const storyText = document.getElementById(`#story-${id}`).textContent;
-
-
 function storyTone() {
 
-    const storyCards = [document.querySelector('.card-body')];
-
+    const storyCards = document.querySelectorAll('.card-body');
 
     for (let i = 0; i < storyCards.length; i++) {
         const story = storyCards[i];
-        console.log('story:', story);
         const sentValue = story.getAttribute('data-sentiment');
-        console.log('sentValue', sentValue);
 
-        if (sentValue < -3) {
+        if (sentValue <= -3) {
             story.classList.add('gradient1');
-        } else if (sentValue > -3 && sentValue < -1) {
+        } else if (sentValue > -3 && sentValue <= -0.5) {
             story.classList.add('gradient2');
-        } else if (sentValue > -1 && sentValue < 1) {
+        } else if (sentValue > -0.5 && sentValue <= 0) {
             story.classList.add('gradient3');
-        } else if (sentValue > 1 && sentValue < 3) {
+        } else if (sentValue > 0 && sentValue <= 2) {
             story.classList.add('gradient4');
-        } else if (sentValue > 3) {
+        } else if (sentValue > 2 && sentValue <= 3) {
             story.classList.add('gradient5');
+        } else if (sentValue > 3) {
+            story.classList.add('gradient6');
         }
-
-
-
-        // switch (sentValue) {
-        //     case (sentValue < -3):
-        //         storyText.classList.add('gradient1');
-        //         break;
-        //     case (sentValue > -3 && sentValue < -1):
-        //         storyText.classList.add('gradient2');
-        //         break;
-        //     case (sentValue > -1 && sentValue < 1):
-        //         storyText.classList.add('gradient3');
-        //         break;
-        //     case (sentValue > 1 && sentValue < 3):
-        //         storyText.classList.add('gradient4');
-        //         break;
-        //     case (sentValue > 3 && sentValue < 5):
-        //         storyText.classList.add('gradient5');
-        //         break;
-        // }
     }
 }
 
